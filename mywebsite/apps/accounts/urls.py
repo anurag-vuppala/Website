0c5e0488640eed02a5312af3ebf610f7c2cec72a
anurag-vuppala <75685045+anurag-vuppala@users.noreply.github.com>
2021-08-25 22:38:08 +0100

    3rd update

--- mywebsite/apps/accounts/urls.py
+++ mywebsite/apps/accounts/urls.py
@@ -6,7 +6,7 @@ app_name="accounts"
 urlpatterns = [
     path("profile", views.ProfileView.as_view(), name="profile"),
     # log in
-    path("login",auth_views.LoginView.as_view(template_name="accounts/apps/login.html"),name="login"),
+    path("login",auth_views.LoginView.as_view(template_name="accounts/login.html"),name="login"),
     path("logout", auth_views.LogoutView.as_view(), name="logout"),
 ]
 
