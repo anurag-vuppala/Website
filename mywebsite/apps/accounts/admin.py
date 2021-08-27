dda87716a9b23dcff4c95d9d966dfb65cf6634b0
anurag-vuppala <75685045+anurag-vuppala@users.noreply.github.com>
2021-08-27 21:15:12 +0100

    4rd update

--- mywebsite/apps/accounts/admin.py
+++ mywebsite/apps/accounts/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import UserPersona, UserProfile
+from .models import UserPersona, UserProfile, UserInterest
 
 admin.site.register(UserProfile)
 admin.site.register(UserPersona)
+admin.site.register(UserInterest)
+
+
